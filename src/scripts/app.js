47d00a92955c2807eb60d2127753f20a566f2d1a
goSnowbank <sycbbb@sina.com>
2017-06-01 16:54:51 +0800

    bandow

---
+++ src/scripts/app.js
@@ -0,0 +1,4 @@
+import '../../src/sass/index.scss';
+window.onload=function(){
+	console.log(1);
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const extractSass = new ExtractTextPlugin({
+    filename: "styles/[name].[contenthash:5].css",
+    disable: process.env.NODE_ENV === "development"
+});
+const config={
+	context: path.resolve(__dirname, './src'),
+    entry: {
+        app:'./scripts/app.js',
+    },
+    output:{
+        path: path.resolve(__dirname, './dist'),
+        filename: 'scripts/[name]-[chunkhash:5].js',
+    },
+    module: {
+        rules: [
+         	{
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [ "html-loader" ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    fallback: "style-loader",
+                    publicPath:"../"              //真是个坑。坑死人了
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                use: [
+                  'url-loader?limit=3500&name=images/[name]-[hash:5].[ext]',
+                  'image-webpack-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+       new htmlWebpackPlugin({
+            filename:'index.html',
+            template:'views/index.html',
+            inject:'body',  //指定js放那个位子 比如body
+            chunks:['app'],   //指定那个js
+       }),
+       
+       //转化sass的方法
+       extractSass
+    ]
+}
+module.exports=config;
